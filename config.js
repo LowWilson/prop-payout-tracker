18b8f94cb77bf63707a7475c6f47e0d547864dc3
LowWilson <42088406+LowWilson@users.noreply.github.com>
2026-06-12 18:14:39 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Supabase設定
 // Supabase Project Settings > API からコピーして貼る
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_URL = "https://wwlwaaudyyerluzvwqdw.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind3bHdhYXVkeXllcmx1enZ3cWR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEyNTA3NjIsImV4cCI6MjA5NjgyNjc2Mn0.LW-cBlESlFC65HuMEHroPILvZDkwrJ0mgYiEmJRDQok";
